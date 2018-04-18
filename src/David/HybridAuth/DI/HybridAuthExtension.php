483f9d5d7c9e27846f9e3f90462c2235894a2f41
David Kastanek <kastanekdavid@gmail.com>
2018-04-18 14:15:35 +0200

    Remove copyright

--- src/David/HybridAuth/DI/HybridAuthExtension.php
+++ src/David/HybridAuth/DI/HybridAuthExtension.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @author David Kaštánek <kastanekdavid@gmail.com>
- * @copyright Copyright (c) 2016, David Kaštánek
- */
-
 namespace David\HybridAuth\DI;
 
 class HybridAuthExtension extends \Nette\DI\CompilerExtension
--- src/David/HybridAuth/Manager.php
+++ src/David/HybridAuth/Manager.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * @author David Kaštánek <kastanekdavid@gmail.com>
- * @copyright Copyright (c) 2016, David Kaštánek
- */
-
 namespace David\HybridAuth;
 
 use Nette\SmartObject;
